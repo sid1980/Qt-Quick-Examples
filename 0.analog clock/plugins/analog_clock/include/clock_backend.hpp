ac3e7dc415220e00980987c01ef47c72dfbe6044
DIODEELEGIA <SyplessEleki@gmail.com>
2021-06-03 00:21:12 +0900

    fix all license

--- 0.analog clock/plugins/analog_clock/include/clock_backend.hpp
+++ 0.analog clock/plugins/analog_clock/include/clock_backend.hpp
@@ -1,17 +1,21 @@
-/*--------------------------------------------------------------------------
-	Copyright © 2021, diode
-	
-	This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
+/*-------------------------------------------------------------------------------
+                           Copyright (c) 2021 DIODE.
+    
+    This file is part of analog clock.
+    
+    analog clock is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    This program is distributed in the hope that it will be useful,
+
+    analog clock is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*///-----------------------------------------------------------------------
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with analog clock.  If not, see <https://www.gnu.org/licenses/>.
+*///-----------------------------------------------------------------------------
 #pragma once
 
 #include <QObject>
